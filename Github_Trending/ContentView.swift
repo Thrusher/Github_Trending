55a1d975065d3ef3289ebf8cd461ff8bd08e6c4c
Patryk Drozd <drozd.patryk89@gmail.com>
2020-09-20 09:54:53 +0200

    Initial Commit

---
+++ Github_Trending/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github_Trending
+//
+//  Created by Patryk Drozd on 20/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Github_Trending/Github_TrendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_TrendingApp.swift
+//  Github_Trending
+//
+//  Created by Patryk Drozd on 20/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Github_TrendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
